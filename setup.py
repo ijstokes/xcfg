f11bd983f50e88e8d18f73e5e584789207787198
Ian Stokes-Rees <alumni.uwaterloo.ca>
2011-11-15 16:26:00 -0500

    Changed project URL to github

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='Extended Configurator',
       description='An alternative to ConfigParser for managing configuration from XML, JSON, INI, key/value, environment, etc.',
       author='Ian Stokes-Rees',
       author_email='ijstokes@spmetric.com',
-      url='http://portal.nebiogrid.org/devel/projects/xcfg/',
+      url='https://github.com/ijstokes/xcfg',
       py_modules=['xcfg'],
      )
